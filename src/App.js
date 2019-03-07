2981c8bc544a658ddb08b9a2ef19ff5f0ca1294a
Your Name <wamontgomery0068@gmail.com>
2019-03-07 13:17:43 -0600

    Progressing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Luke from './Images/Luke.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App_Container">
+      <div className = "App_Container">
+        <div className = "Card_Container">
+          <div className = "Card_Top_Inner_Box">
+            <div className = "Card_Top_Left"></div>
+            <div className = "Card_Top_Right">
+              <img className = "Card_Image" src = { Luke } />
+            </div>
+          </div>
+          <div className = "Card_Bottom_Inner_Box"></div>
+        </div>
       </div>
     );
   }
